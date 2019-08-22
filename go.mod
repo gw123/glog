4202f42b2f0e1372f406e0bbe3e8d838e5c14449
gw123 <963353840@qq.com>
2019-08-22 14:41:15 +0800

    add color

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gw123/glog
+
+go 1.12
+
+require (
+	github.com/jinzhu/gorm v1.9.10
+	github.com/pkg/errors v0.8.1
+)
--- log.go
+++ log.go
@@ -9,6 +9,24 @@ import (
 	"strings"
 )
 
+const (
+	ColorBlack       = 0
+	ColorRed         = 1
+	ColorGreen       = 2
+	ColorYello       = 3
+	ColorBule        = 4
+	ColorAmaranth    = 5
+	ColorUltramarine = 6
+	ColorWhite       = 7
+
+	Style0 = 0
+	Style1 = 1
+	Style4 = 4
+	Style5 = 5
+	Style7 = 7
+	Style8 = 8
+)
+
 func SetOutput() {
 
 }
@@ -19,22 +37,64 @@ func Dump(input interface{}) {
 		fmt.Println("Dump", err.Error())
 		return
 	}
-	output(string(out1))
+	logColorful(string(out1), ColorUltramarine, ColorUltramarine, 1)
 }
 
-func Error(out string) {
-	log.Printf("%s", out)
+func Error(format string, other ...interface{}) {
+	//log.Printf("%s", out)
+	str := fmt.Sprintf(format, other)
+	logColorful("[E] "+str, ColorBlack, ColorRed, Style1)
 }
 
-func output(out string) {
-	_, file, line, ok := runtime.Caller(2)
+func Warn(format string, other ...interface{}) {
+	//log.Printf("%s", out)
+	str := fmt.Sprintf(format, other)
+	logColorful("[W] "+str, ColorBlack, ColorYello, Style1)
+}
+
+func Info(format string, other ...interface{}) {
+	//log.Printf("%s", out)
+	str := fmt.Sprintf(format, other)
+	logColorful("[I] "+str, ColorBlack, ColorUltramarine, Style1)
+}
+
+func Debug(format string, other ...interface{}) {
+	//log.Printf("%s", out)
+	str := fmt.Sprintf(format, other)
+	logColorful("[D] "+str, ColorBlack, ColorAmaranth, Style1)
+}
+
+func Output(out string) {
+	_, file, line, ok := runtime.Caller(3)
 	if ok {
 		gopath, _ := os.LookupEnv("GOPATH")
 		gopath = strings.Replace(gopath, "\\", "/", -1)
 		file = strings.Replace(file, gopath+"/src/", "", -1)
+		strs := strings.Split(file, "/")
+		if len(strs) > 3 {
+			strs = strs[len(strs)-2:]
+		}
+		file = strings.Join(strs, "/")
 		log.Printf("[%s %d] %s", file, line, out)
 	} else {
 		fmt.Println("Dump caller 1 err")
 	}
 	return
+}
+
+func logColorful(out string, bg, fg, style int) {
+	if runtime.GOOS == "linux" {
+		bg = bg % 10
+		fg = fg % 10
+		style = style % 10
+
+		bg = bg + 40
+		bg = fg + 30
+
+		format := fmt.Sprintf("%c[%d;%d;%dm%s%s%c[0m\n",
+			0x1B, style, bg, fg, "", out, 0x1B)
+		Output(format)
+	} else {
+		Output(out)
+	}
 }
